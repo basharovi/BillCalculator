1bea6144b401e9bc3bab480a99931e723be1b285
Bashar Ovi <mominur.ovi@savethechildren.org>
2021-03-12 12:19:28 +0600

    Taken Input From User

--- MobileBillCalculator/Program.cs
+++ MobileBillCalculator/Program.cs
@@ -6,7 +6,24 @@ namespace MobileBillCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var inputStartTime = Console.ReadLine();
+                var inputEndTime = Console.ReadLine();
+
+                var startTime = GetConvertedDateTime(inputStartTime);
+                var endTime = GetConvertedDateTime(inputEndTime);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Someting went wrong!  {0}", ex.Message);
+            }
+        }
+
+        private static DateTime GetConvertedDateTime(string inputTime)
+        {
+            return Convert.ToDateTime(inputTime.Split(":")[1]);
         }
     }
 }
